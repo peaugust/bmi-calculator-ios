e0d467bcf03e0aa9cef65970554e5af00f05e666
Pedro Augusto Freddi Rodrigues <pedro.freddi@jungledevs.com>
2019-07-29 16:08:22 -0300

    feat: Add BMI's calculus function and prevent wrong inputs

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -18,9 +18,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func calcBMI() {
+    @IBAction func didTapBMIButton() {
         
-        let alert = UIAlertController(title: "Result", message: "Fields must be filled", preferredStyle: .alert)
+        guard let weight = weightInput.text,
+            let height = heightInput.text,
+            let weightConverted = Double(weight),
+            let heightConverted = Double(height) else {
+                let alert = UIAlertController(title: "Result", message: "Fields must be filled", preferredStyle: .alert)
+                
+                let action = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+                
+                alert.addAction(action)
+                
+                present(alert, animated: true, completion: nil)
+                return
+        }
+        
+        let bmiResult = calcBMI(weight: weightConverted, height: heightConverted)
+        //        TODO: Verify type of BMI
+        let alert = UIAlertController(title: "Result", message: "Your BMI is \(bmiResult)", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Cancel", style: .default, handler: nil)
         
@@ -28,6 +44,10 @@ class ViewController: UIViewController {
         
         present(alert, animated: true, completion: nil)
         
+    }
+    
+    func calcBMI(weight: Double, height: Double) -> Double {
+        return weight / pow(height, 2.0)
     }
     
 }
