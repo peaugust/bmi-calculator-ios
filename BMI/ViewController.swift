c339234a811ded604b2e0854fb78ad749de8b26c
Pedro Augusto Freddi Rodrigues <pedro.freddi@jungledevs.com>
2019-07-29 15:35:51 -0300

    feat: Add UI elements

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weightInput: UITextField!
+    @IBOutlet weak var heightInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func calcBMI() {
+        
+        let alert = UIAlertController(title: "Result", message: "Fields must be filled", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
